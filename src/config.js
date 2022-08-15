69f997ab49f4ea0887694fd9dfc65f31e58966d5
varunsinghDG <85149973+varunsinghDG@users.noreply.github.com>
2022-08-15 15:46:09 +0530

    new server url

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
 /* eslint-disable eslint-comments/disable-enable-pair */
 /* eslint-disable import/prefer-default-export */
- export const serverUrl = "https://salty-scrubland-03771.herokuapp.com/api";
- // export const serverUrl = 'http://localhost:3001/api';
+export const serverUrl = "https://infinite-river-74868.herokuapp.com/api";
+// export const serverUrl = 'http://localhost:3001/api';
