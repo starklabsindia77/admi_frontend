31b9b1ad2fd40f71fb0928934a7a327e18c12f04
varunsinghDG <85149973+varunsinghDG@users.noreply.github.com>
2022-09-18 13:29:25 +0530

    config changes

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 /* eslint-disable eslint-comments/disable-enable-pair */
 /* eslint-disable import/prefer-default-export */
-// export const serverUrl = 'https://infinite-river-74868.herokuapp.com/api';
-export const serverUrl = 'http://localhost:3001/api';
+export const serverUrl = 'https://infinite-river-74868.herokuapp.com/api';
+// export const serverUrl = 'http://localhost:3001/api';
 export const ChatAuthKey = '68cfa559bba3b173dac4bb0b8d8d6d9b0f9ac0c9';
 export const COMETAPP_ID = '219615523591b942';
 export const COMETAPP_REGION = 'us';
